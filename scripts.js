3cc52c527945ea77e9955a6bbbf658c5d7248c01
Nadhorion <67388006+Nadhorion@users.noreply.github.com>
2026-03-07 23:46:19 -0500

    Add number, operator, and equals button logic
    It is now possible to enter a number, operator, and second number
    followed by the equals sign button to calculate. The screen will reflect
    the current number being typed or show the calculated result.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+let numberA = '';
+let numberB = '';
+let operation = '';
+let calculatorScreen = document.querySelector('#screen');
+const calculatorDiv = document.querySelector('#calculator');
+calculatorDiv.addEventListener('click', calculatorInteraction);
+
 function addition(a, b) { return a + b; }
 function subtraction(a, b) { return a - b; }
 function multiplication(a, b) { return a * b; }
@@ -8,25 +15,51 @@ function operate(a, b, operator) {
     switch(operator) {
         case '+':
             return addition(a, b);
-            
         case '-':
             return subtraction(a, b);
-            
         case '*':
             return multiplication(a, b);
-            
         case '/':
             return division(a, b);
-            
         default:
             console.log('Not getting proper operator. Formatting?');
     }
 }
 
-function isProperNumber() {}
+function calculatorInteraction(event) {
 
-let numberA = null;
-let numberB = null;
-let operation = '';
+    let buttonClicked = event.target;
+
+    if (buttonClicked.className === "number") {
+        
+        if (!operation) {
+            numberA += buttonClicked.id;
+            calculatorScreen.textContent = numberA;
+        } else if (operation) {
+            numberB += buttonClicked.id;
+            calculatorScreen.textContent = numberB;
+        }
+
+    } else if (buttonClicked.className === "operator") {
+
+        if (numberA) {
+            operation = buttonClicked.id;
+        }
+
+    } else if (buttonClicked.id === "=") {
+
+        if (numberA && numberB && operation) {
+            let result = operate(+numberA, +numberB, operation);
+            calculatorScreen.textContent = result;
+        }
+
+    }
+    
+    //add number to numberA string
+    //if target is operator, 
+
+}
+
+
 
 
